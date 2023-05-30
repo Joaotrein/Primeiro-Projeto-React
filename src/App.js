1d3f429611aa95c9979538fb7f83a1b1865fe6eb
João Pedro Grahl Trein <84185442+Joaotrein@users.noreply.github.com>
2023-05-30 12:07:44 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header/Header';
+import Menu from './components/Main/Menu';
+import Painel from './components/Main/Painel';
+import Footer from './components/Main/Footer';
+
+function App() {
+  return (
+    <>  
+    <Header/>
+    <Menu/>
+    <Painel/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
